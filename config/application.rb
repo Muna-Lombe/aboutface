3c5d081c56e953661cf01f290ff851dcc2cd639c
Muna-Lombe <3223352545@qq.com>
2021-11-10 05:54:25 +0800

    trying again

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Aboutface
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
   end
 end
